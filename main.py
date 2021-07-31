3a7fc9283f83bff1d858d5a32e81e5ae567ebaa2
Cooper <cwfaber@ucsc.edu>
2021-07-31 13:59:43 -0700

    most audio functionality working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext import tasks
 import random
 import confidential
+import music
 import time
 
 random.seed(a=None, version=2)
@@ -15,15 +16,37 @@ class GeorgeBot(discord.Client):
 
     #command definition
     @moses.command()
+    #joins voice, if message author is in channel
     async def join(message):
-        channel = message.author.voice.channel
-        await channel.connect()
+        if not message.author.voice:
+            await message.channel.send("You aren't in a voice channel, schmuck")
+        else:
+            channel = message.author.voice.channel
+            try:
+                await channel.connect()
+            except:
+                pass
 
     @moses.command()
+    #leaves voice
     async def leave(message):
-        await message.voice_client.disconnect()
+        try:
+            await message.voice_client.disconnect()
+        except:
+            pass
 
-
+    @moses.command()
+    #plays song
+    async def play(channel, url):
+        #try:
+        server = channel.guild
+        voice_channel = server.voice_client
+        async with channel.typing():
+            filename = await music.YTDLSource.from_url("https://www.youtube.com/watch?v=8mHKHKR8x6A", loop=None)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await channel.send('**Now playing:** {}'.format(filename))
+        #except:
+        #    pass
 
 
 
@@ -31,34 +54,33 @@ class GeorgeBot(discord.Client):
     #status on launch
     @GeorgeBot.event
     async def on_ready(self):
-            stati = [
-                "Skynet Online",
-                "Preparing Arnold for a presidential run"
-            ]
-            status = stati[random.randrange(0, (len(stati)), 1)]
-            print ("Starting with status "+ status)
-            game = discord.Game(status)
-            await client.change_presence(status=discord.Status.online, activity=game)
+        stati = [
+            "Skynet Online",
+            "Preparing Arnold for a presidential run"
+        ]
+        status = stati[random.randrange(0, (len(stati)), 1)]
+        print ("Starting with status "+ status)
+        game = discord.Game(status)
+        await client.change_presence(status=discord.Status.online, activity=game)
 
     @GeorgeBot.event
     async def on_message(self, message):
         if message.content.find('good bot') >= 0:
             channel = message.channel
             await channel.send("i live to serve")
-            try: 
-                await client.join(message)
-            except discord.errors.ClientException:
-                pass
+            await client.join(message)
+        
+        if message.content.find('voice test') >= 0:
+            channel = message.channel
+            await channel.send("i'm trying my robotic best")
+            await client.join(message)
+            await client.play(message.channel, message)
 
-            
         if message.content.find('die george') >= 0:
             channel = message.channel
             await channel.send('going to die now')
             await channel.send('you have killed me, master')
-            try: 
-                await client.leave(message)
-            except Exception:
-                pass
+            await client.leave(message)
             await client.logout()
             
 
